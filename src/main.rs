473fac658eba5a50d59f618a2f3995859fe95789
Hadi Hamoud <hadikhamoud@gmail.com>
2026-03-09 16:55:04 +0300

    added file resolution

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-use std::fs;
-use std::io::ErrorKind;
-
 use fast_disambig::utils;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _ = match fs::read_to_string(
-        "/Users/hadihamoud/.camel_tools/data/morphology_db/calima-msa-r13/morphology.db",
-    ) {
-        Ok(content) => content,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => {
-                eprintln!("file not found!");
-                return Err(error.into());
-            }
-            _ => {
-                eprintln!("Other error: {error}");
-                return Err(error.into());
-            }
-        },
+    let camel_dir = match utils::get_camel_dir() {
+        Ok(dir) => dir,
+        Err(e) => {
+            eprintln!("Could not get camel dir: {e}");
+            return Err(e.into());
+        }
     };
+
+    let db_path = camel_dir.join("data/morphology_db/calima-msa-r13/morphology.db");
+    let dediac = utils::dediac_ar(&db_path.to_string_lossy());
+    println!("{dediac}");
+
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,26 @@
 use regex::Regex;
-use std::string::String;
+use std::{env, fs, io, path::PathBuf, string::String};
+
+pub fn get_camel_dir() -> io::Result<PathBuf> {
+    let home_dir = env::var("HOME").map(PathBuf::from).map_err(|_| {
+        io::Error::new(
+            io::ErrorKind::NotFound,
+            "HOME environment variable is not set",
+        )
+    })?;
+
+    let camel_dir = home_dir.join(".camel_tools");
+    if !camel_dir.exists() {
+        fs::create_dir_all(&camel_dir)?;
+    }
+
+    Ok(camel_dir)
+}
 
 pub fn dediac_ar(s: &str) -> String {
     let dediac_re =
-        Regex::new(r"\u064b|\u064c|\u064d|\u064e|\u064f|\u0650|\u0651|\u0652u|\u0671").unwrap();
+        Regex::new(r"[\u{064B}\u{064C}\u{064D}\u{064E}\u{064F}\u{0650}\u{0651}\u{0652}\u{0671}]")
+            .unwrap();
 
     let sub = dediac_re.replace_all(s, "");
     sub.to_string()
