1a2e0bf3d6aeac973ab1f66873e1850a421617e0
Hadi Hamoud <hadikhamoud@gmail.com>
2026-03-13 22:31:56 +0300

    (compression) moved into impl

--- src/downloader.rs
+++ src/downloader.rs
@@ -5,17 +5,12 @@ use anyhow::Result;
 use serde::Deserialize;
 use serde::Serialize;
 use serde_json;
-use serde_json::Value;
 use std::collections::BTreeMap;
-use std::default;
 use std::fs::File;
-use std::fs::exists;
-use std::fs::read_to_string;
 use std::io::BufReader;
 use std::io::BufWriter;
-use std::io::Read;
 use std::path::PathBuf;
-use std::{env, fs, io, string::String};
+use std::{env, fs};
 
 #[derive(Serialize, Deserialize)]
 pub struct CamelCatalogue {
@@ -35,109 +30,96 @@ pub struct CamelResource {
     dependencies: Vec<String>,
 }
 
-pub fn get_or_create_camel_dir() -> Result<PathBuf, io::Error> {
-    let curr_home_dir = match env::home_dir() {
-        Some(path) => path,
-        None => {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::NotFound,
-                "Home directory not found",
-            ));
-        }
-    };
-    let camel_dir = curr_home_dir.join(".camel_tools");
-    if !camel_dir.exists() {
-        fs::create_dir_all(&camel_dir)?;
-    }
-    Ok(camel_dir)
-}
+impl CamelCatalogue {
+    pub fn download(catalogue_path: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+        let catalogue_file = File::create(catalogue_path)?;
+        match ureq::get(constants::CAMEL_DATA_CATALOGUE_URL).call() {
+            Ok(mut response) => {
+                let writer = BufWriter::new(catalogue_file);
 
-fn download_camel_catalogue() -> Result<(), Box<dyn std::error::Error>> {
-    let camel_dir = get_or_create_camel_dir()?;
-    let catalogue_path = camel_dir.join("catalogue.json");
-    let catalogue_file = File::create(catalogue_path)?;
-    match ureq::get(constants::CAMEL_DATA_CATALOGUE_URL).call() {
-        Ok(mut response) => {
-            let writer = BufWriter::new(catalogue_file);
+                let response_json: serde_json::Value =
+                    serde_json::from_str(&response.body_mut().read_to_string()?)?;
+                serde_json::to_writer_pretty(writer, &response_json)?;
+                Ok(())
+            }
 
-            let response_json: serde_json::Value =
-                serde_json::from_str(&response.body_mut().read_to_string()?)?;
-            serde_json::to_writer_pretty(writer, &response_json)?;
-            Ok(())
-        }
-
-        Err(e) => {
-            eprintln!("Error getting Catalogue!");
-            Err(Box::new(e))
+            Err(e) => {
+                eprintln!("Error getting Catalogue!");
+                Err(Box::new(e))
+            }
         }
     }
-}
 
-pub fn get_camel_catalogue() -> Result<CamelCatalogue, Box<dyn std::error::Error>> {
-    let camel_dir = get_or_create_camel_dir()?;
-    let catalogue_path = camel_dir.join("catalogue.json");
-    if !catalogue_path.exists() {
-        download_camel_catalogue()?;
+    pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
+        let camel_dir = get_or_create_camel_dir()?;
+        let catalogue_path = camel_dir.join("catalogue.json");
+        if !catalogue_path.exists() {
+            Self::download(&catalogue_path)?;
+        }
+
+        let reader = BufReader::new(File::open(catalogue_path)?);
+        let mut catalogue_json: CamelCatalogue = serde_json::from_reader(reader)
+            .context("Was not able to read the json into Camel Catalogue")?;
+        catalogue_json.packages.retain(|_, res| !res.private);
+
+        Ok(catalogue_json)
     }
 
-    let reader = BufReader::new(File::open(catalogue_path)?);
-    let catalogue_json: CamelCatalogue = serde_json::from_reader(reader)
-        .context("Was not able to read the json into Camel Catalogue")?;
-    Ok(catalogue_json)
-}
+    pub fn display(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let name_width = self.packages.keys().map(String::len).max().unwrap_or(1);
 
-pub fn load_camel_resources() -> Result<(), Box<dyn std::error::Error>> {
-    let mut catalogue = get_camel_catalogue()?;
-    catalogue.packages.retain(|_, res| !res.private);
-
-    let name_width = catalogue
-        .packages
-        .keys()
-        .map(String::len)
-        .max()
-        .unwrap_or(1);
-
-    let license_width = 10;
-    let size_width = 10;
-    println!(
-        "{:<name_width$}\t{:>size_width$} {:<license_width$}\t{}",
-        "Package Name",
-        "Size",
-        "License",
-        "Description",
-        name_width = name_width,
-        size_width = size_width,
-        license_width = license_width,
-    );
-    println!(
-        "{:_<name_width$}\t{:_>size_width$} {:_<license_width$}\t{:_<11}\n",
-        "",
-        "",
-        "",
-        "",
-        name_width = name_width,
-        size_width = size_width,
-        license_width = license_width,
-    );
-
-    for (k, res) in catalogue.packages.iter() {
-        let name = k;
-        let size = res.size.unwrap_or(0);
-        let mut size_str = "".to_string();
-        if size != 0 {
-            size_str = utils::bytes_to_mib_human_readable(size);
-        }
+        let license_width = 10;
+        let size_width = 10;
         println!(
             "{:<name_width$}\t{:>size_width$} {:<license_width$}\t{}",
-            name,
-            size_str,
-            res.license.as_deref().unwrap_or(""),
-            res.description,
+            "Package Name",
+            "Size",
+            "License",
+            "Description",
             name_width = name_width,
             size_width = size_width,
             license_width = license_width,
         );
-    }
+        println!(
+            "{:_<name_width$}\t{:_>size_width$} {:_<license_width$}\t{:_<11}\n",
+            "",
+            "",
+            "",
+            "",
+            name_width = name_width,
+            size_width = size_width,
+            license_width = license_width,
+        );
 
-    Ok(())
+        for (k, res) in self.packages.iter() {
+            let name = k;
+            let size = res.size.unwrap_or(0);
+            let mut size_str = "".to_string();
+            if size != 0 {
+                size_str = utils::bytes_to_mib_human_readable(size);
+            }
+            println!(
+                "{:<name_width$}\t{:>size_width$} {:<license_width$}\t{}",
+                name,
+                size_str,
+                res.license.as_deref().unwrap_or(""),
+                res.description,
+                name_width = name_width,
+                size_width = size_width,
+                license_width = license_width,
+            );
+        }
+
+        Ok(())
+    }
+}
+
+pub fn get_or_create_camel_dir() -> Result<PathBuf> {
+    let curr_home_dir = env::home_dir().context("Home directory not found")?;
+
+    let camel_dir = curr_home_dir.join(".camel_tools");
+    if !camel_dir.exists() {
+        fs::create_dir_all(&camel_dir).context("Could not create the .camel_tools directory")?;
+    }
+    Ok(camel_dir)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,12 @@ use fast_disambig::downloader;
 use fast_disambig::utils;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let camel_dir = match downloader::get_or_create_camel_dir() {
-        Ok(dir) => dir,
-        Err(e) => {
-            eprintln!("Could not get camel dir: {e}");
-            return Err(e.into());
-        }
-    };
+    let camel_dir = downloader::get_or_create_camel_dir()?;
 
     let db_path = camel_dir.join("data/morphology_db/calima-msa-r13/morphology.db");
     let dediac = utils::dediac_ar(&db_path.to_string_lossy());
-    downloader::load_camel_resources()?;
-    println!("{dediac}");
+    let catalogue = downloader::CamelCatalogue::load()?;
+    catalogue.display()?;
 
     Ok(())
 }
